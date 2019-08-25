52721e8e6f4725e8262752f6df0c5de418bf2934
vrunge <vincent.Runge@univ-evry.fr>
2019-08-25 22:18:01 +0200

    paperGraph update

--- R/paperGraphs.R
+++ R/paperGraphs.R
@@ -7,14 +7,98 @@
 #' @param nb the number of the Figure in paper
 #' @return a graph
 #'
-paperGraph <- function(nb)
+paperGraph <- function(nb, penalty = 0, decay = 1, oneValue = 0)
 {
-  mygraph <- graph()
-  if(nb == 1)
+  mygraph <- NULL
+  if(nb == 4)
   {
-
+    mygraph <- graph(type = "std", penalty = penalty)
   }
-
+  if(nb == 5)
+  {
+    mygraph <- graph(type = "isotonic", penalty = penalty, decay = decay)
+  }
+  if(nb == 6)
+  {
+    mygraph <- graph(
+      Edge("Dw", "Dw", type = "null", decay = decay),
+      Edge("Up", "Up", type = "null", decay = decay),
+      Edge("Down", "Up", type = "up", penalty = penalty),
+      Edge("Up", "Down", type = "down", penalty = penalty),
+      StartEnd(start = "Dw")
+    )
+  }
+  if(nb == 7)
+  {
+    mygraph <- graph(type = "isotonic", penalty = penalty, decay = decay, K = Inf)
+  }
+  if(nb == 8)
+  {
+    mygraph <- graph(
+      Edge("1", "2", type = "std", penalty = 0),
+      Edge("2", "3", type = "std", penalty = 0),
+      Edge("1", "1", type = "null", penalty = 0),
+      Edge("2", "2", type = "null", penalty = 0),
+      Edge("2", "3", type = "null", penalty = 0),
+      StartEnd(start = "1", end = "3")
+    )
+  }
+  if(nb == 9)
+  {
+    mygraph <- graph(
+      Edge("Wait", "Seg", type = "null", penalty = 0),
+      Edge("Seg", "Seg", type = "null", penalty = 0),
+      Edge("Seg", "Wait", type = "std", penalty = penalty),
+      StartEnd(start = "Wait", end = "Seg")
+    )
+  }
+  if(nb == 12)
+  {
+    mygraph <- graph(type = "relevant", penalty = penalty)
+  }
+  if(nb == 17)
+  {
+    mygraph <- graph(
+      Edge("Wait1", "Wait2", type = "null", penalty = 0),
+      Edge("Wait2", "Seg", type = "null", penalty = 0),
+      Edge("Seg", "Seg", type = "null", penalty = 0),
+      Edge("Seg", "Wait1", type = "std", penalty = penalty),
+      StartEnd(start = "Wait1", end = "Seg")
+    )
+  }
+  if(nb == 18)
+  {
+    mygraph <- graph(
+      Edge("Dw1", "Dw", type = "null", penalty = 0),
+      Edge("Dw", "Dw", type = "null", penalty = 0),
+      Edge("Dw", "Up1", type = "up", penalty = penalty),
+      Edge("Up1", "Up", type = "null", penalty = 0),
+      Edge("Up", "Dw1", type = "down", penalty = penalty),
+      StartEnd(start = "Dw1", end = "Dw")
+    )
+  }
+  if(nb == 19)
+  {
+    mygraph <- graph(
+      Edge("Dw", "Dw", type = "null", penalty = 0),
+      Edge("Dw", "Dw", type = "down", penalty = 0),
+      Edge("Dw", "Up", type = "up", penalty = penalty),
+      Edge("Up", "Up", type = "null", penalty = 0),
+      Edge("Up", "Dw", type = "down", penalty = 0),
+      StartEnd(start = "Dw", end = "Dw")
+    )
+  }
+  if(nb == 20)
+  {
+    mygraph <- graph(
+      Edge("mu0", "mu0", type = "null", penalty = 0, K = K),
+      Edge("mu0", "Coll", type = "std", penalty = penalty),
+      Edge("Coll", "Coll", type = "null"),
+      Edge("Coll", "mu0", type = "std",  K = K),
+      StartEnd(start = "mu0", end = c("mu0","Dw")),
+      Node("mu0", min = oneValue, max = oneValue)
+    )
+  }
   return(mygraph)
 }
 
