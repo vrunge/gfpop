e7640b948954a508a9b1f7fcdcf1d6e74847b40a
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-11-18 12:34:22 -0700

    sn tests fail

--- tests/testthat/test-sn.R
+++ tests/testthat/test-sn.R
@@ -14,36 +14,45 @@ sngraph <- function(n.segs, type, gap){
   s <- n.segs-1
   null.vec <- 0:s
   seg.vec <- 1:s
+  edge.df <- gfpop::Edge(
+    paste0("seg", seg.vec-1), paste0("seg", seg.vec), type, gap=gap)
   gfpop::graph(
-    gfpop::StartEnd(start=0, end=s),
-    gfpop::Edge(null.vec, null.vec, "null"),
-    if(1 < n.segs)gfpop::Edge(seg.vec-1, seg.vec, type, gap=gap),
+    gfpop::StartEnd(start=paste0("seg", 0), end=paste0("seg", s)),
+    gfpop::Edge(paste0("seg", null.vec), paste0("seg", null.vec), "null"),
+    if(1 < n.segs)edge.df else edge.df[0,],
     all.null.edges=FALSE)
 }
 
-test_that("absSup model with 1 segment returned", {
-  g1 <- sngraph(1L, "absSup", 1)
+test_that("abs model with 1 segment returned", {
+  g1 <- sngraph(1L, "abs", 1)
   fit1 <- gfpop::gfpop(
     profile614chr2$probes$logratio,
     mygraph = g1, type = "mean")
   expect_identical(fit1$changepoints, nrow(profile614chr2$probes))
   expect_equal(fit1$parameters, mean(profile614chr2$probes$logratio))
+  expect_identical(fit1$states, "seg0")
 })
 
-test_that("absSup model with 2 segments returned", {
+test_that("abs model with 2 segments returned", {
   g2 <- sngraph(2L, "abs", 1)
   fit2 <- gfpop::gfpop(
     profile614chr2$probes$logratio,
     mygraph = g2, type = "mean")
   expect_identical(length(fit2$changepoints), 2L)
+  expect_identical(fit2$changepoints[2], nrow(profile614chr2$probes))
+  expect_identical(length(fit2$parameters), 2L)
+  expect_identical(fit2$states, c("seg0", "seg1"))
 })
 
-test_that("absSup model with 3 segments returned", {
+test_that("abs model with 3 segments returned", {
   g3 <- sngraph(3L, "abs", 1)
   fit3 <- gfpop::gfpop(
     profile614chr2$probes$logratio,
     mygraph = g3, type = "mean")
   expect_identical(length(fit3$changepoints), 3L)
+  expect_identical(fit3$changepoints[3], nrow(profile614chr2$probes))
+  expect_identical(length(fit3$parameters), 3L)
+  expect_identical(fit3$states, c("seg0", "seg1", "seg2"))
 })
 
 test_that("std model with 1 segment returned", {
@@ -53,6 +62,7 @@ test_that("std model with 1 segment returned", {
     mygraph = g1, type = "mean")
   expect_identical(fit1$changepoints, nrow(profile614chr2$probes))
   expect_equal(fit1$parameters, mean(profile614chr2$probes$logratio))
+  expect_identical(fit1$states, "seg0")
 })
 
 test_that("std model with 2 segments returned", {
@@ -61,6 +71,9 @@ test_that("std model with 2 segments returned", {
     profile614chr2$probes$logratio,
     mygraph = g2, type = "mean")
   expect_identical(length(fit2$changepoints), 2L)
+  expect_identical(fit2$changepoints[2], nrow(profile614chr2$probes))
+  expect_identical(length(fit2$parameters), 2L)
+  expect_identical(fit2$states, c("seg0", "seg1"))
 })
 
 test_that("std model with 3 segments returned", {
@@ -69,30 +82,7 @@ test_that("std model with 3 segments returned", {
     profile614chr2$probes$logratio,
     mygraph = g3, type = "mean")
   expect_identical(length(fit3$changepoints), 3L)
-})
-
-sngraph2 <- function(n.segs, type, gap){
-  stopifnot(is.integer(n.segs), length(n.segs)==1, n.segs >= 1)
-  s <- n.segs-1
-  seg.vec <- 1:s
-  gfpop::graph(
-    gfpop::StartEnd(start=0, end=s),
-    if(1 < n.segs)gfpop::Edge(seg.vec-1, seg.vec, type, gap=gap),
-    all.null.edges=TRUE)
-}
-
-test_that("all.null=TRUE std model with 2 segments returned", {
-  g2 <- sngraph2(2L, "std", 0)
-  fit2 <- gfpop::gfpop(
-    profile614chr2$probes$logratio,
-    mygraph = g2, type = "mean")
-  expect_identical(length(fit2$changepoints), 2L)
-})
-
-test_that("all.null=TRUE std model with 3 segments returned", {
-  g3 <- sngraph2(3L, "std", 0)
-  fit3 <- gfpop::gfpop(
-    profile614chr2$probes$logratio,
-    mygraph = g3, type = "mean")
-  expect_identical(length(fit3$changepoints), 3L)
+  expect_identical(fit3$changepoints[3], nrow(profile614chr2$probes))
+  expect_identical(length(fit3$parameters), 3L)
+  expect_identical(fit3$states, c("seg0", "seg1", "seg2"))
 })
