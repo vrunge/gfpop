15c82ada79e19288fbe66844cf32601bb870fb1b
Vincent Runge <vincent.runge@univ-evry.fr>
2019-12-04 22:29:01 +0100

    Applications.Rmd comment first example

--- tests/testthat/test-sn.R
+++ tests/testthat/test-sn.R
@@ -8,24 +8,31 @@ data(profile614chr2, package="gfpop")
 ### reduce data size
 profile614chr2$probes <- profile614chr2$probes[1:10000,]
 
-
 g <- gfpop::graph(type="std")
 x <- rnorm(10)
 gfpop::gfpop(x, g)
 
-sngraph <- function(n.segs, type, gap){
+######
+###### sngraph function
+######
+
+sngraph <- function(n.segs, type, gap)
+{
   stopifnot(is.integer(n.segs), length(n.segs)==1, n.segs >= 1)
   s <- n.segs-1
   null.vec <- 0:s
   seg.vec <- 1:s
-  edge.df <- gfpop::Edge(
-    paste0("seg", seg.vec-1), paste0("seg", seg.vec), type, gap=gap)
+  edge.df <- gfpop::Edge(paste0("seg", seg.vec-1), paste0("seg", seg.vec), type, gap=gap)
   gfpop::graph(
     gfpop::StartEnd(start=paste0("seg", 0), end=paste0("seg", s)),
     gfpop::Edge(paste0("seg", null.vec), paste0("seg", null.vec), "null"),
     if(1 < n.segs)edge.df else edge.df[0,],
     all.null.edges=FALSE)
 }
+
+######
+###### tests with abs, 1,2,3 segments
+######
 
 test_that("abs model with 1 segment returned", {
   g1 <- sngraph(1L, "abs", 1)
@@ -58,6 +65,10 @@ test_that("abs model with 3 segments returned", {
   expect_identical(length(fit3$parameters), 3L)
   expect_identical(fit3$states, c("seg0", "seg1", "seg2"))
 })
+
+######
+###### tests with std, 1,2,3 segments
+######
 
 test_that("std model with 1 segment returned", {
   g1 <- sngraph(1L, "std", 0)
