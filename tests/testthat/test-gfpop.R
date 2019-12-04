61a0c8a9f9586a155701b1a85136de1ca5fd36d4
Vincent Runge <vincent.runge@univ-evry.fr>
2019-12-04 22:08:24 +0100

    adding penalty = in test-gfpop

--- tests/testthat/test-gfpop.R
+++ tests/testthat/test-gfpop.R
@@ -5,15 +5,15 @@ context("graph")
 data(ECG, package="gfpop")
 test_that("gfpop returns character states", {
   myGraph <- graph(
-    Edge("beforeQ", "Q",      "down", 80000000, gap=0),
-    Edge("Q",       "R",      "up",   0,        gap=2000),
-    Edge("R",       "S",      "down", 0,        gap=5000),
-    Edge("S",       "S1",     "up",   0,        gap=2000),
-    Edge("S1",      "S2",     "up",   0,        gap=1000),
-    Edge("S2",      "peak",   "up",   0,        gap=0),
-    Edge("peak",    "after",  "down", 0,        gap=0),
-    Edge("after",   "last",   "down", 0,        gap=0),
-    Edge("last",    "beforeQ","up",   0,        gap=0), all.null.edges = TRUE)
+    Edge("beforeQ", "Q",      "down", penalty = 80000000, gap=0),
+    Edge("Q",       "R",      "up",   penalty = 0,        gap=2000),
+    Edge("R",       "S",      "down", penalty = 0,        gap=5000),
+    Edge("S",       "S1",     "up",   penalty = 0,        gap=2000),
+    Edge("S1",      "S2",     "up",   penalty = 0,        gap=1000),
+    Edge("S2",      "peak",   "up",   penalty = 0,        gap=0),
+    Edge("peak",    "after",  "down", penalty = 0,        gap=0),
+    Edge("after",   "last",   "down", penalty = 0,        gap=0),
+    Edge("last",    "beforeQ","up",   penalty = 0,        gap=0), all.null.edges = TRUE)
   fit <- gfpop(ECG$data$millivolts, mygraph = myGraph, type = "mean")
   expect_true(all(fit$states %in% myGraph$state1))
 })
